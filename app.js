f319d0c95eaa1b13cd08dcf804ad31f5bd0c9f4f
fayezmmm <fayezmorsel8@gmail.com>
2025-07-19 19:28:06 +0300

    validation email

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById("submit").addEventListener("click", function () {
-    // name
+  // name
   const nameInput = document.getElementById("name").value.trim();
   const errorName = document.querySelector(".error-name");
 
@@ -17,7 +17,7 @@ document.getElementById("submit").addEventListener("click", function () {
   }
   errorName.textContent = "";
 
-//   last name 
+  //   last name
   const lastNameInput = document.getElementById("last-name").value.trim();
   const errorLastName = document.querySelector(".error-last-name");
 
@@ -25,13 +25,29 @@ document.getElementById("submit").addEventListener("click", function () {
     errorLastName.textContent = "Last Name cannot be empty.";
     return;
   }
-  if (/\d/.test(lastNameInput)){
+  if (/\d/.test(lastNameInput)) {
     errorLastName.textContent = "Last Name connot contain numbers.";
     return;
   }
-  if (lastNameInput.length < 5 || lastNameInput.length >12) {
-    errorLastName.textContent = "Last Name must be between 5 and 12 characters."
-    return
+  if (lastNameInput.length < 5 || lastNameInput.length > 12) {
+    errorLastName.textContent =
+      "Last Name must be between 5 and 12 characters.";
+    return;
   }
   errorLastName.textContent = "";
+
+  //   email
+  const emailInput = document.getElementById("email").value.trim();
+  const errorEmail = document.querySelector(".error-email");
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  if (emailInput === "") {
+    errorEmail.textContent = "Email cannot be empty.";
+    return;
+  }
+  if(!emailRegex.test(emailInput)){
+    errorEmail.textContent = " Please enter a valid email address.";
+    return;
+  }
+  errorEmail.textContent = "";
 });
